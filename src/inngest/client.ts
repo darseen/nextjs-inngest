67fc708a76a0062fd8f7142cae7058b5bc5772a2
darseen <darseen.dev@gmail.com>
2026-04-02 18:43:29 +0300

    Add env variables

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,3 +1,8 @@
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest({ id: "my-app" });
+export const inngest = new Inngest({
+  id: "my-app",
+  baseUrl: process.env.INNGEST_BASE_URL,
+  signingKey: process.env.INNGEST_SIGNING_KEY,
+  eventKey: process.env.INNGEST_EVENT_KEY,
+});
